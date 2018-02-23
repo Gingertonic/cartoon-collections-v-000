e0cdf63088ffa8833437b0ba2ab02a5fc7dac1c8
Beth Schofield <bethmschofield@gmail.com>
2018-02-23 01:18:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,25 +12,23 @@ def long_planeteer_calls(calls)
   calls.any? {|call| call.length > 4}
 end
 
-#The find_the_cheese method should accept an array of strings. 
-#It should then look through these strings to find and return the first string that is a type of cheese. 
+#The find_the_cheese method should accept an array of strings.
+#It should then look through these strings to find and return the first string that is a type of cheese.
 #The types of cheese that appear are "cheddar", "gouda", and "camembert".
 #NOT WORKING
-def find_the_cheese(yummies)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types. {|cheese| yummies.find(cheese)}
-end
-
-#ALSO NOT WORKING
-def find_the_cheese(yummies)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.map |cheese| 
-    yummies.find(cheese)
-  end
+# def find_the_cheese(yummies)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   cheese_types. {|cheese| yummies.find(cheese)}
+# end
+# 
+# #ALSO NOT WORKING
+# def find_the_cheese(yummies)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   cheese_types.map |cheese|
+#     yummies.find(cheese)
+#   end
 
   def find_the_cheese(yummies)
       cheese_types = ["cheddar", "gouda", "camembert"]
       cheese_types.each {|cheese| yummies.include?(cheese)}
   end
-      
-    
