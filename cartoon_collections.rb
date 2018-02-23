c61ab52c9a2ea40443cc5e5bfb23945210ba0a7e
Beth Schofield <bethmschofield@gmail.com>
2018-02-23 02:18:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,18 @@
-# require "pry"
-#
-# def roll_call_dwarves(dwarves)
-#   dwarves.each_with_index do |dwarf, index|
-#     puts "#{index + 1}. #{dwarf}"
-#   end
-# end
-#
-# def summon_captain_planet(calls)
-#   calls.map {|call| "#{call.capitalize}!"}
-# end
-#
-# def long_planeteer_calls(calls)
-#   calls.any? {|call| call.length > 4}
-# end
+require "pry"
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
+end
+
+def summon_captain_planet(calls)
+  calls.map {|call| "#{call.capitalize}!"}
+end
+
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4}
+end
 
 #WORKING WITHOUT INCLUDE
 # def find_the_cheese(yummies)
