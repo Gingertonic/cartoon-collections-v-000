1e4d3ab296732d215394c5dceeeaa2955f250d94
Beth Schofield <bethmschofield@gmail.com>
2018-02-23 02:17:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,15 @@
 # require "pry"
-# 
+#
 # def roll_call_dwarves(dwarves)
 #   dwarves.each_with_index do |dwarf, index|
 #     puts "#{index + 1}. #{dwarf}"
 #   end
 # end
-# 
+#
 # def summon_captain_planet(calls)
 #   calls.map {|call| "#{call.capitalize}!"}
 # end
-# 
+#
 # def long_planeteer_calls(calls)
 #   calls.any? {|call| call.length > 4}
 # end
